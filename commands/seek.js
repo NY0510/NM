4d128988f9e28c196682e09499f1c13de6d5fa41
NY64 <me@ny64.kr>
2022-11-29 18:05:20 +0900

    seek

--- commands/seek.js
+++ commands/seek.js
@@ -25,16 +25,29 @@ module.exports = {
 			});
 
 		const time = args[0].split(":");
-		const seconds = time[0] * 60 + time[1] * 1;
-
-		if (isNaN(seconds))
+		if (time.length > 3)
 			return message.reply({
-				embeds: [new EmbedBuilder().setDescription(`${process.env.EMOJI_X} **시간을 입력해주세요**`).setColor(process.env.COLOR_ERROR)],
+				embeds: [new EmbedBuilder().setDescription(`${process.env.EMOJI_X} **올바른 형식으로 시간을 입력해주세요**`).setColor(process.env.COLOR_ERROR)],
 			});
 
-		player.seek(seconds * 1000);
-		message.reply({
-			embeds: [new EmbedBuilder().setDescription(`**\`${args[0]}\`**으로 이동했어요!`).setColor(process.env.COLOR_NORMAL)],
-		});
+		let seconds = 0;
+		if (time.length === 1) seconds = Number(time[0]);
+		if (time.length === 2) seconds = Number(time[0]) * 60 + Number(time[1]);
+		if (time.length === 3) seconds = Number(time[0]) * 3600 + Number(time[1]) * 60 + Number(time[2]);
+
+		if (seconds > player.queue.current.duration / 1000)
+			return message.reply({
+				embeds: [new EmbedBuilder().setDescription(`${process.env.EMOJI_X} **올바른 형식으로 시간을 입력해주세요**`).setColor(process.env.COLOR_ERROR)],
+			});
+
+		try {
+			player.seek(player.position + seconds * 1000);
+		} catch (RangeError) {
+			return message.reply({
+				embeds: [new EmbedBuilder().setDescription(`${process.env.EMOJI_X} **올바른 형식으로 시간을 입력해주세요**`).setColor(process.env.COLOR_ERROR)],
+			});
+		}
+
+		return message.reply({ embeds: [new EmbedBuilder().setDescription(`⏩ **${args[0]}**초 앞으로 이동했어요!`).setColor(process.env.COLOR_NORMAL)] });
 	},
 };
