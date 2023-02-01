a910b368ceb46c414549a2bb98e6a94116e8d570
Doyun <ydy01092007@gmail.com>
2023-02-02 00:51:12 +0900

    add remove command

--- commands/remove.js
+++ commands/remove.js
@@ -12,7 +12,7 @@ module.exports = {
         if (!channel)
         {
             return message.reply({
-				embeds: [new EmbedBuilder().setDescription(`${process.env.EMOJI_X} **음성 채널에 먼저 접속하세요!**`).setColor(process.env.COLOR_ERROR)],
+				egimbeds: [new EmbedBuilder().setDescription(`${process.env.EMOJI_X} **음성 채널에 먼저 접속하세요!**`).setColor(process.env.COLOR_ERROR)],
 			});
         }
         if (!player)
