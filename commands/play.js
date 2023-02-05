c15a575ba11f3c617ba41fab3972b7683dfebca2
NY64 <me@ny64.kr>
2023-02-05 23:08:41 +0900

    기본볼륨 조절

--- commands/play.js
+++ commands/play.js
@@ -22,7 +22,7 @@ module.exports = {
 			voiceChannel: channel.id,
 			textChannel: message.channel.id,
 			selfDeafen: true,
-			volume: 50,
+			volume: 70,
 		});
 		const bindChannel = client.channels.cache.get(player.textChannel);
 
