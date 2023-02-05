2da2f26bd519bb8d5560700c51216606e9b243c6
NY64 <me@ny64.kr>
2023-02-05 22:50:38 +0900

    🐛 빈 메세지 보내는 버그 수정

--- commands/remove.js
+++ commands/remove.js
@@ -6,32 +6,30 @@ module.exports = {
 	aliases: ["rm", "그"],
 
 	run: async (client, message, args) => {
-		const { channel } = message.member.voice
-        const player = client.manager.get(message.guild.id);
+		const { channel } = message.member.voice;
+		const player = client.manager.get(message.guild.id);
 
-        if (!channel)
-        {
-            return message.reply({
-				egimbeds: [new EmbedBuilder().setDescription(`${process.env.EMOJI_X} **음성 채널에 먼저 접속하세요!**`).setColor(process.env.COLOR_ERROR)],
+		if (!channel) {
+			return message.reply({
+				embeds: [new EmbedBuilder().setDescription(`${process.env.EMOJI_X} **음성 채널에 먼저 접속하세요!**`).setColor(process.env.COLOR_ERROR)],
 			});
-        }
-        if (!player)
-        return message.reply({
-            embeds: [new EmbedBuilder().setDescription(`${process.env.EMOJI_X} **이 서버에서 재생중인 음악이 없어요**`).setColor(process.env.COLOR_ERROR)],
-        });
+		}
+		if (!player)
+			return message.reply({
+				embeds: [new EmbedBuilder().setDescription(`${process.env.EMOJI_X} **이 서버에서 재생중인 음악이 없어요**`).setColor(process.env.COLOR_ERROR)],
+			});
 
-        const index = (Object.keys(args).length == 0) ? 0 : args - 1
-        if (0 <= index && index < player.queue.size) {
-            const title = player.queue[index].title
-            player.queue.remove(index)
-            return message.reply({
-                embeds: [new EmbedBuilder().setDescription(`${process.env.EMOJI_CHECK} **${title} 삭제 완료**`).setColor(process.env.COLOR_NORMAL)],
-            });
-        }
-        else {
-            return message.reply({
-                embeds: [new EmbedBuilder().setDescription(`${process.env.EMOJI_X} **존재하지 않는 번호에요**`).setColor(process.env.COLOR_ERROR)],
-            });
-        }
-	}
+		const index = Object.keys(args).length == 0 ? 0 : args - 1;
+		if (0 <= index && index < player.queue.size) {
+			const title = player.queue[index].title;
+			player.queue.remove(index);
+			return message.reply({
+				embeds: [new EmbedBuilder().setDescription(`${process.env.EMOJI_CHECK} **${title} 삭제 완료**`).setColor(process.env.COLOR_NORMAL)],
+			});
+		} else {
+			return message.reply({
+				embeds: [new EmbedBuilder().setDescription(`${process.env.EMOJI_X} **존재하지 않는 번호에요**`).setColor(process.env.COLOR_ERROR)],
+			});
+		}
+	},
 };
