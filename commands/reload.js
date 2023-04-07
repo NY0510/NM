fac2a4d86e59595a12c1efb6c9da2c50262f88dd
NY64 <me@ny64.kr>
2023-04-07 21:02:31 +0900

    add reload

--- commands/reload.js
+++ commands/reload.js
@@ -16,11 +16,11 @@ module.exports = {
 
 		if (!client.commands.has(args[0]))
 			return await message.reply({
-				embeds: [new EmbedBuilder().setDescription(`**${process.env.EMOJI_X} ${args[0]} 모듈은 존재하지 않아요**`).setColor(process.env.COLOR_ERROR)],
+				embeds: [new EmbedBuilder().setDescription(`**${process.env.EMOJI_X} \`${args[0]}\` 모듈은 존재하지 않아요**`).setColor(process.env.COLOR_ERROR)],
 			});
 
 		await message
-			.reply({ embeds: [new EmbedBuilder().setTitle(`**<a:loading:1088966142560841789> ${args[0]} 모듈 리로드 중**`).setColor(process.env.COLOR_NORMAL)] })
+			.reply({ embeds: [new EmbedBuilder().setTitle(`**<a:loading:1088966142560841789> \`${args[0]}\` 모듈 리로드 중**`).setColor(process.env.COLOR_NORMAL)] })
 			.then(async msg => {
 				try {
 					delete require.cache[require.resolve(`./${args[0]}.js`)];
@@ -31,12 +31,12 @@ module.exports = {
 					msg.edit({
 						embeds: [
 							new EmbedBuilder()
-								.setDescription(`**${process.env.EMOJI_X} ${args[0]} 모듈을 리로드 하는 도중 문제가 발생했어요**\n` + "```\n" + String(e.stack) + "\n```")
+								.setDescription(`**${process.env.EMOJI_X} \`${args[0]}\` 모듈을 리로드 하는 도중 문제가 발생했어요**\n` + "```\n" + String(e.stack) + "\n```")
 								.setColor(process.env.COLOR_ERROR),
 						],
 					});
 				}
-				msg.edit({ embeds: [new EmbedBuilder().setTitle(`**<a:loading:1088966142560841789> ${args[0]} 모듈 리로드 완료!**`).setColor(process.env.COLOR_NORMAL)] });
+				msg.edit({ embeds: [new EmbedBuilder().setTitle(`**<a:loading:1088966142560841789> \`${args[0]}\` 모듈 리로드 완료!**`).setColor(process.env.COLOR_NORMAL)] });
 			});
 	},
 };
