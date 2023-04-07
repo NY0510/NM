69b790203a714a8a7927870d86e3533615c250df
NY64 <me@ny64.kr>
2023-04-07 20:38:26 +0900

    fix prograssbar

--- utils/progressBar.js
+++ utils/progressBar.js
@@ -2,11 +2,11 @@ module.exports.progressBar = player => {
 	//[ ●▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ ]
 	const progress = (Math.floor(player.position / 1000) / Math.floor(player.queue.current.duration / 1000)) * 100;
 	let bar = "";
-	for (let i = 0; i != Math.floor(progress / 10); i++) {
+	for (let i = 0; i != Math.floor((progress * 1.5) / 10); i++) {
 		bar += "▬";
 	}
 	bar += "●";
-	for (let i = 10; i != Math.floor(progress / 10); i--) {
+	for (let i = 15; i != Math.floor((progress * 1.5) / 10); i--) {
 		bar += "▬";
 	}
 	return `**[ ${bar} ]**`;
