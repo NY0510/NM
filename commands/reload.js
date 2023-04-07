6c9fb196510acdb4cbec75e1fdabe64544f4d3ff
NY64 <me@ny64.kr>
2023-04-07 20:50:25 +0900

    add reload

--- commands/reload.js
+++ commands/reload.js
@@ -9,13 +9,19 @@ module.exports = {
 		const ownerID = process.env.BOT_OWNER_ID;
 
 		if (message.author.id !== ownerID) return;
-		if (!args[0]) return message.channel.send(new EmbedBuilder().setTitle("Error").setDescription("Please provide a command to reload."));
-		if (!client.commands.has(args[0])) return message.channel.send(new EmbedBuilder().setTitle("Error").setDescription(`The command \`${args[0]}\` doesn't seem to exist.`));
+		if (!args[0])
+			return message.channel.send({ embeds: [new EmbedBuilder().setTitle("Error").setDescription(`Please specify a command to reload.`).setColor(process.env.COLOR_ERROR)] });
+
+		if (!client.commands.has(args[0]))
+			return message.channel.send({
+				embeds: [new EmbedBuilder().setTitle("Error").setDescription(`The command \`${args[0]}\` doesn't seem to exist.`).setColor(process.env.COLOR_ERROR)],
+			});
+
 		delete require.cache[require.resolve(`./${args[0]}.js`)];
 		client.commands.delete(args[0]);
 
 		client.commands.set(args[0], require(`./${args[0]}.js`));
-		message.channel.send(new EmbedBuilder().setTitle("Success").setDescription(`The command \`${args[0]}\` has been reloaded.`));
+		message.channel.send({ embeds: [new EmbedBuilder().setTitle(`Command \`${args[0]}\` has been reloaded!`).setColor(process.env.COLOR_NORMAL)] });
 
 		console.log(`Command \`${args[0]}\` has been reloaded.`);
 	},
