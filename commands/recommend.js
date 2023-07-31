a8f21cf991b725437c1658d0bb7cbc3693188378
NY64 <me@ny64.kr>
2023-08-01 01:26:59 +0900

    asdf

--- commands/recommend.js
+++ commands/recommend.js
@@ -28,7 +28,8 @@ module.exports = {
 				embeds: [new EmbedBuilder().setDescription(`${process.env.EMOJI_X}} **현재 재생중인 곡이 유튜브 영상이 아니여서 해당 기능 이용이 불가능해요`)],
 			});
 
-		const recommendVideos = await youtubesearchapi.GetVideoDetails(nowPlayingYoutubeId).suggestion[0];
+		console.log("aaaaaaaaaa");
+		const recommendVideos = await youtubesearchapi.GetVideoDetails(nowPlayingYoutubeId).suggestion;
 		console.log(recommendVideos);
 
 		let videoCount = 0;
