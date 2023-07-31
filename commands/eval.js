4c1819322e1cd78f3bd67279475c1554d207ee4e
NY64 <me@ny64.kr>
2023-07-31 22:13:09 +0900

    a

--- commands/eval.js
+++ commands/eval.js
@@ -9,6 +9,8 @@ module.exports = {
 		async function clean(text) {
 			if (text && text.constructor.name == "Promise") text = await text;
 
+			if (typeof text !== "string") text = require("util").inspect(text, { depth: 1 });
+
 			text = text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
 
 			return text;
