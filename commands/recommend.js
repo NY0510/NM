9514bdef030f45683bc3ffff6995efdf64d093f0
NY64 <me@ny64.kr>
2023-08-01 01:14:15 +0900

    asdf

---
+++ commands/recommend.js
@@ -0,0 +1,47 @@
+require("dotenv").config();
+const { EmbedBuilder } = require("discord.js");
+const youtubesearchapi = require("youtube-search-api");
+
+module.exports = {
+	name: "recommend",
+	aliases: ["추천", "suggestion", "ㄱㄷ채ㅡㅡ둥", "r"],
+
+	run: async (client, message, args) => {
+		const player = client.manager.get(message.guild.id);
+
+		if (!player || !player?.queue?.current?.title)
+			return message.reply({
+				embeds: [new EmbedBuilder().setDescription(`${process.env.EMOJI_X} **이 서버에서 재생중인 음악이 없어요**`).setColor(process.env.COLOR_ERROR)],
+			});
+
+		const { channel } = message.member.voice;
+
+		if (channel.id !== player.voiceChannel)
+			return message.reply({
+				embeds: [new EmbedBuilder().setDescription(`${process.env.EMOJI_X} **저와 같은 음성채널에 접속해 있지 않아요**`).setColor(process.env.COLOR_ERROR)],
+			});
+
+		const nowPlayingYoutubeId = player.queue.current.uri.split("?v=")[1];
+
+		if (!nowPlayingYoutubeId)
+			return message.reply({
+				embeds: [new EmbedBuilder().setDescription(`${process.env.EMOJI_X}} **현재 재생중인 곡이 유튜브 영상이 아니여서 해당 기능 이용이 불가능해요`)],
+			});
+
+		const recommendVideos = await youtubesearchapi.GetVideoDetails(nowPlayingYoutubeId).suggestion;
+
+		let videoCount = 0;
+		recommendVideos.forEach(async video => {
+			await player.search(`https://youtube.com/watch?v=${video.id}`, message.author);
+			videoCount++;
+		});
+
+		message.channel.send({
+			embeds: [
+				new EmbedBuilder()
+					.setDescription(`${process.env.EMOJI_CHECK} **현재 재생중인 곡을 기반으로 한 추천 노래 ${videoCount}개를 추가했어요**`)
+					.setColor(process.env.COLOR_NORMAL),
+			],
+		});
+	},
+};
