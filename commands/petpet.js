c18a954cd0728ab90ef8a1307a906fbecf6a9244
NY64 <me@ny64.kr>
2023-08-04 16:57:34 +0900

    ad

--- commands/petpet.js
+++ commands/petpet.js
@@ -6,7 +6,7 @@ module.exports = {
 	aliases: ["pet", "쓰담", "ㅔㄷ섿ㅅ", "ㅔㄷㅅ"],
 
 	run: async (client, message, args) => {
-		let animatedGif = await petPetGif(message.author.avatarURL({ extension: "png", size: 256 }));
-		await message.reply({ files: [animatedGif] });
+		let animatedGif = await petPetGif(message.author.avatarURL({ extension: "png", size: 512 }));
+		await message.reply({ files: [{ file: animatedGif, name: "pet.gif" }] });
 	},
 };
