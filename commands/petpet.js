ddbf7df1a8f3795a38db59af352802f6083ffae3
NY64 <me@ny64.kr>
2023-08-04 17:47:52 +0900

    await

--- commands/petpet.js
+++ commands/petpet.js
@@ -7,15 +7,13 @@ module.exports = {
 	run: async (client, message, args) => {
 		let user = message.mentions.users.first() || message.author;
 
-		// Get the user's avatar URL
 		const avatarURL = user.avatarURL({ extension: "png", size: 512 });
 
-		// Generate the petpet gif and send it in the message
 		try {
 			let animatedGif = await petPetGif(avatarURL);
 			await message.reply({ files: [{ attachment: animatedGif, name: "pet.gif" }] });
 		} catch (error) {
-			return message.reply("`ERROR` " + error.message);
+			return await message.reply("`ERROR` " + error.message);
 		}
 	},
 };
