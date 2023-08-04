ed06f40e43b413fae40ac8a2a219c8541aa3ec99
NY64 <me@ny64.kr>
2023-08-04 16:34:23 +0900

    pet

---
+++ commands/petpet.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const petPetGif = require("pet-pet-gif");
+
+module.exports = {
+	name: "petpet",
+	aliases: ["pet", "쓰담", "ㅔㄷ섿ㅅ", "ㅔㄷㅅ"],
+
+	run: async (client, message, args) => {
+		let animatedGif = await petPetGif(message.author.avatarURL);
+		console.log(animatedGif);
+	},
+};
