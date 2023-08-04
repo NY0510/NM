a8eb7b040ab41e56e116c6c8bb622201b72cf9ae
NY64 <me@ny64.kr>
2023-08-04 16:36:03 +0900

    a

--- commands/petpet.js
+++ commands/petpet.js
@@ -8,5 +8,6 @@ module.exports = {
 	run: async (client, message, args) => {
 		let animatedGif = await petPetGif(message.author.avatarURL);
 		console.log(animatedGif);
+		await message.reply("a");
 	},
 };
